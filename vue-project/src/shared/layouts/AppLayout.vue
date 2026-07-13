82709a837a3caad761c74ba49dcfbb2953257d07
MAlmeida3734 <macunhadealmeida@gmail.com>
2026-07-13 14:41:37 -0300

    master

---
+++ vue-project/src/shared/layouts/AppLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+    <div>
+        <aside>Menu</aside>
+        <main>Main</main>
+    </div>
+</template>
