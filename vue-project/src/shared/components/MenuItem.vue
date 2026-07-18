254891ebae66ed8ab77dfc0665e1968ba758dabf
MAlmeida3734 <macunhadealmeida@gmail.com>
2026-07-18 02:59:42 -0300

    master

--- vue-project/src/shared/components/MenuItem.vue
+++ vue-project/src/shared/components/MenuItem.vue
@@ -8,7 +8,7 @@ defineProps<MenuItemProps>();
 </script>
 
 <template>
-<RouterLink :to="to">
+<RouterLink :to="to" class="px-4 py-2 text-zinc-300 hover:text-white hover:bg-white/5 rounded-md">
     {{ label }}
 </RouterLink>
 </template>
